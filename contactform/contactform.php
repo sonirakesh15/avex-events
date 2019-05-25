4ecac3db51e3bb1d921144143c747bf3d91f84ea
rakesh <rakesh.soni@venturepact.com>
2019-05-25 16:26:11 +0530

    [project]: upload

---
+++ contactform/contactform.php
@@ -0,0 +1,16 @@
+<?php
+if($_POST){
+    $hospital_name = $_POST['hospital_name'];
+    $state = $_POST['state'];
+    $district = $_POST['district'];
+    $person_name = $_POST['person_name'];
+    $mobileNo = $_POST['mobileNo'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $finalMsg= "Hospital Name : ".$hospital_name." State: ".$state." District: ".$district." Name: ".$person_name." MobileNo:".$mobileNo." Email: ".$email." Message: ".$message ;
+
+//send email
+    mail("rakesh.soni@venturepact.com", "This is an email from:" .$email, $finalMsg);
+}
+?>
